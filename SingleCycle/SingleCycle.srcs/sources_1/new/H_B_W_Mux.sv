de9497b6695d7e95aa33fd3855c6c0dc076864ad
234082 <31855609+234082@users.noreply.github.com>
2023-11-02 15:57:48 -0500

    register file initial design

---
+++ SingleCycle/SingleCycle.srcs/sources_1/new/H_B_W_Mux.sv
@@ -0,0 +1,11 @@
+`timescale 1ns / 1ps
+
+module H_B_W_Mux(
+    input wire logic H_B_W, 
+    input [31:0] Reg2_Value, 
+    output [31:0] bits_to_store 
+    );
+    
+    
+    
+endmodule
---
+++ SingleCycle/SingleCycle.srcs/sources_1/new/RegisterFile.sv
@@ -0,0 +1,33 @@
+`timescale 1ns / 1ps
+
+// there are 32 entries in the register file 
+
+module RegisterFile(
+    input wire logic CLK, // not sure if clock is necessarily a wire (from the perspective of the reg file, it is) 
+    input wire logic write_register, 
+    input wire logic [4:0] Register1, Register2, DestinationRegister, // 2 ^ 5 = 32 entries 
+    input wire logic [31:0] write_register_data, 
+    output logic [31:0] Reg1_SrcA, Reg2_SrcB  
+    );
+    
+    reg [31:0] registers [31:0]; // 32 bit, 32-entry register file 
+    
+    // should initialize all regs that are hard coded (e.g. R0 always = 0)
+    initial begin 
+        
+    end 
+    
+    // we will just be constantly reading, reading doesn't rely on clock or any signal 
+    always_comb begin 
+        Reg1_SrcA = registers[Register1]; 
+        Reg2_SrcB = registers[Register2];  
+    end 
+    
+    // at every clock edge we're checking for write register signal 
+    always_ff @ (posedge CLK) begin 
+        if (write_register == 1'b1 && DestinationRegister != 5'd0) begin // we can't write to register 0 
+            registers[DestinationRegister] <= write_register_data;  
+        end  
+    end 
+    
+endmodule
